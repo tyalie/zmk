33c99998c46f2d008edeed11c361a16682fee7da
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-01-15 02:03:51 +0100

    fix: jlink runner had an unknown target selected
    I only noticed that while I tried to use the JLINK debug target.
    Seemingly JLINK requires a more specific target than what I used before.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- app/boards/arm/tyk380/board.cmake
+++ app/boards/arm/tyk380/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 
 board_runner_args(pyocd "--target=nrf52832")
-board_runner_args(jlink "--device=nrf52832" "--speed=4000")
+board_runner_args(jlink "--device=nrf52832_xxAB" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
